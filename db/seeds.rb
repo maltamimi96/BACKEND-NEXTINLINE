f0dc6109ed10f1953c43dfda228a976bb3a87a5a
maltamimi96 <46971446+maltamimi96@users.noreply.github.com>
2022-07-25 17:23:31 +1000

    adds foreign key to stores

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-
 
 if User.count==0
     User.create(username:"tim",email:"s90six@gmail.com",password: "pword1",password_confirmation:"pword1",admin: true)
@@ -22,5 +13,11 @@ if Store.count==0
     # User.create(username:"tim4",email:"s93six@gmail.com",password: "pword4",password_confirmation:"pword4")
 end
 
+if Client.count==0
+    Client.create(first_name:"John",last_name:"Doe",phone:"0412345678",email:"john.doe@gmail.com", store_id:1)
+    # User.create(username:"tim2",email:"s91six@gmail.com",password: "pword2",password_confirmation:"pword2")
+    # User.create(username:"tim3",email:"s92six@gmail.com",password: "pword3",password_confirmation:"pword3")
+    # User.create(username:"tim4",email:"s93six@gmail.com",password: "pword4",password_confirmation:"pword4")
+end
 
 
